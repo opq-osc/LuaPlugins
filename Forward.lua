cba77946a53ded669660e89dca15ad4b0a28aa10
xjl <1348200269@qq.com>
2020-05-17 17:47:53 +0800

    转发图片

--- Forward.lua
+++ Forward.lua
@@ -8,18 +8,17 @@ function ReceiveFriendMsg(CurrentQQ, data)
 end
 function ReceiveGroupMsg(CurrentQQ, data)
 	if data.FromGroupId == 757360354 then --QQgroup
-		if data.FromUserId == 1752951672 then --QQuser
+		if data.FromUserId == 1702485633 then --QQuser
 	str = json.decode(data.Content)
 	img_url = str.GroupPic[1].Url
 	fileMd5 = str.GroupPic[1].FileMd5
+	picBase64 = str.GroupPic[1].ForwordBuf
 	log.notice('data2-->%s', fileMd5)
 	log.notice('data-->%s', img_url)
-	log.notice('data.GroupId-->%s', data.FromGroupId)
-	log.notice('data.GroupUserQQ-->%s', data.FromUserId)
 		Api.Api_SendMsg(--调用发消息的接口
 		    CurrentQQ,
 		    {
-		        toUser = 578111062, --回复想要转发的群ID
+		        toUser = 921860347, --回复想要转发的群ID
 		        sendToType = 2, --2发送给群1发送给好友3私聊
 		        sendMsgType = "PicMsg", --进行文本复读回复
 		        groupid = 0, --不是私聊自然就为0咯
@@ -27,7 +26,6 @@ function ReceiveGroupMsg(CurrentQQ, data)
 						picUrl = "",
 						picBase64Buf = "",
 						fileMd5 = fileMd5,
-						flashPic = true,
 		        atUser = 0
 		    }
 		)
