05b60a38d2042693c11844eeb57b43a359436d34
XJL <xxxx@xxxx.com>
2021-04-13 17:56:43 +0800

    update

---
+++ RainbowFart.lua
@@ -0,0 +1,45 @@
+-- https://api.lovelive.tools/api/SweetNothings/WebSite/1
+local log = require("log")
+local Api = require("coreApi")
+local json = require("json")
+local http = require("http")
+
+function ReceiveFriendMsg(CurrentQQ, data)
+    return 1
+end
+function ReceiveGroupMsg(CurrentQQ, data)
+
+if string.find(data.Content, "彩虹屁") == 1 then
+    response, error_message =
+        http.request(
+        "GET",
+        "https://nmsl.shadiao.app/api.php?level=min&lang=zh_cn",
+        {
+            headers = {
+                Accept = "*/*"
+            }
+        }
+    )
+    local html = response.body
+    -- local info = json.decode(html)
+    -- local msg = info[1].content
+    log.notice("------ %s",html)
+    luaMsg =
+            Api.Api_SendMsg(--调用发消息的接口
+            CurrentQQ,
+            {
+                toUser = data.FromGroupId, --回复当前消息的来源群ID
+                sendToType = 2, --2发送给群1发送给好友3私聊
+                sendMsgType = "TextMsg", --进行文本复读回复
+                groupid = 0, --不是私聊自然就为0咯
+                content = html, --回复内容
+                atUser = 0 --是否 填上data.FromUserId就可以复读给他并@了
+            }
+        )
+		end
+    return 1
+end
+function ReceiveEvents(CurrentQQ, data, extData)
+    return 1
+end
+	
